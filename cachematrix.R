0ae9013badf716bc702e30f67d661768983dbc53
Your Name HereJoy <jwu84@jhu.edu>
2015-06-21 13:55:54 -0400

    assignment 2

--- cachematrix.R
+++ cachematrix.R
@@ -1,1 +1,29 @@
-# Myproject
+source("http://d396qusza40orc.cloudfront.net/rprog%2Fscripts%2Fsubmitscript1.R")
+makeCacheMatrix <- function(x = matrix())
+{
+  s <- NULL
+  set <- function(y)
+  {
+    x <<- y
+    s <<- NULL
+  }
+  get <- function() x
+  setsolve <- function(solve) s <<- solve
+  getsolve <- function() s
+  list(set = set, get = get, setsolve = setsolve, getsolve = getsolve)
+}
+
+
+
+cacheSolve <- function(x, ...)
+{
+  s <- x$getsolve()
+  if(!is.null(s))
+  {
+    message("getting cached inverse matrix")
+    return(s)
+  }
+  s <- solve(x$get())
+  x$setsolve(s)
+  return(s)
+}
